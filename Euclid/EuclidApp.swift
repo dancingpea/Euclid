55c17e7d68e0e8900b7d6f69df3b165a41f6aa94
@dancing_pea <katarcis@outlook.com>
2026-04-02 21:51:11 +0200

    Initial Commit

---
+++ Euclid/EuclidApp.swift
@@ -0,0 +1,17 @@
+//
+//  EuclidApp.swift
+//  Euclid
+//
+//  Created by Thomas Forget on 02/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct EuclidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EuclidTests/EuclidTests.swift
@@ -0,0 +1,19 @@
+//
+//  EuclidTests.swift
+//  EuclidTests
+//
+//  Created by Thomas Forget on 02/04/2026.
+//
+
+import Testing
+@testable import Euclid
+
+struct EuclidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
